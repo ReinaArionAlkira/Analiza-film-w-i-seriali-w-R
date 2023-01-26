82d6500d209585187892f2c6b0c18a2b829f41d6
ReinaArionAlkira <Renatis.wes00@gmail.com>
2023-01-26 18:21:28 +0100

    Trochę nowego czyszczenia danych, Poprawki poprzednich wykresów, Wykresy z date_added

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Projekt na przedmiot Języki Skryptowe w Analizie Danych
 
 Wykorzystana baza: netflix_titles.csv 
 Źródło: https://www.kaggle.com/datasets/shivamb/netflix-shows?datasetId=434238&searchQuery=R  
+Ostatnia aktualizacja bazy: 27.09.2021r
 Data pobrania: 22.01.2023r
--- RProject.R
+++ RProject.R
@@ -4,6 +4,7 @@ install.packages("treemap")
 install.packages("tidyverse")
 install.packages("ggrepel")
 
+library(lubridate)
 library(tidyverse) # metapackage with lots of helpful functions
 library(ggrepel)
 library(treemap)
@@ -16,7 +17,6 @@ df = data.frame(read.csv("netflix_titles.csv", header=TRUE, sep=",", dec="."), s
 # Na wstępie od razu widzimy że id nam nie będzie potrzebne, więc można się go pozbyć
 df = df[,-1]
 
-
 # Sprawdzenie w których kolumnach znajdują się puste komórki
 dfx = which(df == "", arr.ind = T)
 dfx = table(dfx[, 2])
@@ -24,8 +24,11 @@ dfx = table(dfx[, 2])
 dfBlanks <- data.frame(NumerKolumny = colnames(df[as.numeric(names(dfx))]), IloscPowtorzen = as.numeric(dfx))
 
 # Wykres znalezionych pustych pól
-ggplot(dfBlanks, aes(x = NumerKolumny, y = IloscPowtorzen, fill = NumerKolumny)) + 
+ggplot(dfBlanks, aes(x = reorder(NumerKolumny, IloscPowtorzen), y = IloscPowtorzen, fill = NumerKolumny)) + 
   geom_col() +  
+  labs(x = "Kolumny", y = "IloscPowtorzen") + 
+  ggtitle("Ilość braków w kolumnach") +
+  theme(legend.position = "none") +
   scale_fill_manual(values = c(rep("#3366CC", 6))) +
   geom_text_repel(aes(label = stat(y), group = factor(NumerKolumny)), position = position_nudge_repel(y = 1))
 
@@ -38,12 +41,15 @@ ggplot(dfBlanks, aes(x = NumerKolumny, y = IloscPowtorzen, fill = NumerKolumny))
 # w przypadku kategorii można użyć 3 opcji
 
 # uzupełnianie pustych pól, aby nie musieć usuwać całych wierszy
+# Po dokładniejszym zastanowieniu: w naszej analizie nie wykorzystamy w żaden sposób kolumn cast oraz director
+# Więc zamiast uzupełniać dane w tych kolumnach, można się ich po prostu pozbyć
 
-df$director[df$director == ""] = "Brak danych"
-df$cast[df$cast == ""] = "Brak danych"
+df = df[, -3]
+df = df[, -3]
 df$duration[df$duration == ""] = "Brak danych"
 df$country[df$country == ""] = "Brak danych"
 df$date_added[df$date_added == ""] = "Brak danych"
+df$date_added = trimws(df$date_added)
 
 
 names(sort(table(df$rating)))
@@ -68,9 +74,10 @@ names(table(df$country))[1]
 names(table(df$country))[2]
 df$country <- sub(', ', "", df$country)
 
-summary(df)
-
-
+dfx = which(df == "", arr.ind = T)
+dfx = table(dfx[, 2])
+dfx
+# Pozbyliśmy się wszystkich braków danych
 
 # Sprawdzanie liczby unikalnych wartości, by wiedzieć co warto wziąć na tapetę
 
@@ -79,13 +86,15 @@ uniques <- apply(df, MARGIN = 2, FUN = function(x) length(unique(x)))
 uniques <- data.frame(Kolumny = names(uniques), Unikalne = uniques, stringsAsFactors =  F)
 
 
-ggplot(uniques, aes(x = reorder(Kolumny, Unikalne), y = Unikalne, fill = Kolumny)) +
+ggplot(uniques, aes(y = reorder(Kolumny, Unikalne), x = Unikalne, fill = Kolumny)) +
   geom_col() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-  scale_fill_manual(values = c(rep("#3366CC",3),"#0000AA",rep("#3366CC",5),"#0000AA", "#0000AA","#3366CC")) + 
+  ggtitle("Ilość unikalnych wartości w kolumnach") +
+  theme(plot.title = element_text(size=20, face="bold"),) +
   theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  scale_fill_manual(values = c(rep("#3366CC",2), "#0000AA", rep("#3366CC",4), "#0000AA", "#3366CC")) + 
   labs(x = "Kolumny", y = "Unikalne wartości") + 
-  geom_text_repel(aes(label = stat(y), group = factor(Unikalne)), position = position_nudge_repel(y = 1))
+  geom_text_repel(aes(label = stat(x), group = factor(Unikalne)), position = position_nudge_repel(x = 4))
 
 # Jak widać kolumny:
 # title, oraz description (zaznaczone innym kolorem)
@@ -166,4 +175,78 @@ treemap(moviesRating,
         fontsize.labels=c(15,12), fontcolor.labels=c("white"), palette = c("#0000AA", "#3366CC", "#6633FF"))
 
 
+# przygotowanie danych do wykresu z dodawania treści na przestrzeni lat
 
+#chyba zbędne
+#plotYear <- count(df, date_added)
+#plotTvYear <- count(tvShows, date_added)
+#plotMoviesYear <- count(movies, date_added)
+
+splitedDate <- sub('.*, ', '', df$date_added)
+plotYear <- data.frame(splitedDate)
+plotYear <- count(plotYear, splitedDate)
+
+plotYear <- slice(plotYear, 1:(n() - 1)) 
+
+splitedDateTv <- sub('.*, ', '', tvShows$date_added)
+plotTvYear <- data.frame(splitedDateTv)
+plotTvYear <- count(plotTvYear, splitedDateTv)
+
+plotTvYear <- slice(plotTvYear, 1:(n() - 1))
+
+splitedDateMovies <- sub('.*, ', '', movies$date_added)
+plotMoviesYear <- data.frame(splitedDateMovies)
+plotMoviesYear <- count(plotMoviesYear, splitedDateMovies)
+
+plot(plotYear$splitedDate, plotYear$n,  type = "b", col = 2 , lwd = 3, pch = 1, xlab = "Year", ylab = "Amount")
+lines(plotMoviesYear$splitedDate, plotMoviesYear$n, type = "b", col = 4 , lwd = 3, pch = 1)
+lines(plotTvYear$splitedDate, plotTvYear$n, type = "b", col = 3 , lwd = 3, pch = 1)
+
+legend(x = "topleft",legend = c("All", "Movies", "Tv Shows"), lty = 1, col = c(2, 4, 3), lwd = 3)
+title("Dodawanie treści na przestrzeni lat")
+text(plotYear$splitedDate[-8:0], plotYear$n[-8:0] + 55, labels=plotYear$n[-8:0], col = 1)
+text(plotMoviesYear$splitedDate[-8:0], plotMoviesYear$n[-8:0] + 60, labels=plotMoviesYear$n[-8:0], col = 1)
+text(plotTvYear$splitedDate[-4:0], plotTvYear$n[-4:0] -60, labels=plotTvYear$n[-4:0], col = 1)
+
+# Zauważalny wzrost zawartości rozpoczął się w 2015 roku
+# Wzrost liczby filmów w serwisie Netflix jest znacznie wyższy niż w przypadku programów telewizyjnych
+# W latach 2018: 2020 co roku dodano ponad 1200 nowych filmów
+
+# A w którym miesiącu najczęściej były te treści dodawane?
+splitedMonth <- sub(' .*', '', df$date_added)
+plotMonth <- data.frame(splitedMonth)
+plotMonth <- count(plotMonth, splitedMonth)
+plotMonth <- slice(plotMonth, which(plotMonth$splitedMonth != "Brak")) 
+
+plotMonth$num = as.character(
+  factor(
+    plotMonth$splitedMonth,
+    levels = month.name,
+    labels = c(1:12)
+  )
+)
+
+plotMonth <- plotMonth[order(as.numeric(plotMonth$num)),]
+
+ggplot(data=plotMonth ,aes(x = reorder(splitedMonth, n), y = n, fill = n)) +
+  geom_bar(stat = "identity",width = 1,colour = "white",size = 0.7) +
+  coord_polar()+
+  scale_fill_gradient(low = "white", high = "dodgerblue3") +
+  xlab("")+ylab("") +
+  ggtitle("Dodawanie treści na miesiącach") + 
+  theme(plot.title = element_text(size=20, face="bold"),) + 
+  guides(fill=guide_legend(title="Amount"))
+
+# Jak widać najczęściej treść na Netflixie pojawiała się w Grudniu oraz Lipcu
+
+
+
+# Przeanalizujmy teraz w których latach najczęściej powstawała zawartość dodana na Netflixa
+# Filmy
+
+
+# seriale
+
+
+
+# Podział gatunków
